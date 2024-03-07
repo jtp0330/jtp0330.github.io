e76ddd22781a9ee83c0f18455b8a7c0421c48cae
jtp0330 <jtp0330@gmail.com>
2024-03-06 18:13:07 -0800

    added bg color and border color to cards

--- src/assets/components/projects_showcase/Project.jsx
+++ src/assets/components/projects_showcase/Project.jsx
@@ -1,19 +1,20 @@
 import react from 'react'
 import projectstyle from '../..//css/Project.module.css'
 import Card from 'react-bootstrap/Card'
+
 const Project = (props) => {
 
     //destructuring
     const { projectName, projectDescription, projectLink } = props;
 
     return (
-        <Card className={projectstyle.project_content}>
+        <Card className={`${projectstyle.project_content} hover-zoom hover-shadow`} bg="dark" border="light">
             <Card.Img variant="top" src="#" alt="Project picture" />
             <Card.Body>
                 <div className="project-text">
                     <Card.Title><h3 id="project_header">{projectName}</h3></Card.Title>
                     <Card.Text>
-                        <p>{projectDescription}</p>
+                        {/* <p>{projectDescription}</p> */}
                         <a href={projectLink}>{projectLink}</a>
                     </Card.Text>
                 </div>

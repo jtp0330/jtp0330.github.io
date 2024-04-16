c18e5f4719e98568a7653b07aa9e18f981f18643
jtp0330 <jtp0330@gmail.com>
2024-04-15 22:37:15 -0700

    adding intersection observer to welcome page for dynamic scrolling;

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,10 +1,31 @@
-import react from 'react'
+import { useState } from 'react'
+import { useInView, InView } from 'react-intersection-observer'
 import homestyle from '../css/Home.module.css'
+//utilize intersection observer to display welcome, then your infor card
+
+
 function Home() {
+
+    const observer = new IntersectionObserver((sections) => {
+        sections.forEach((section) => {
+            console.log(section)
+            if (section.isIntersecting) {
+                section.target.classList.add('screenVisible')
+            }
+            else {
+                section.target.classList.remove('screenVisible')
+            }
+        }
+        )
+    });
+    const invisibleElements = document.querySelectorAll(".screenHidden")
+    invisibleElements.forEach((element) => { observer.observe(element) });
+
+
     return (
         <div className={homestyle.home_page}>
-            <h1>Welcome</h1>
-            <div className={homestyle.introduction}>
+            <h1 className={homestyle.screenVisible}>Welcome</h1>
+            <div className={homestyle.introduction + ' ' + homestyle.screenHidden}>
                 <img src="./src/assets/images/Portrait.jpeg" alt="My Portrait" width="400" height="550"></img>
                 <div>
                     <p>My name, is Jared Park.

37a7c825baed111f2f5b0fd2d6d6ffa20e609f00
jtp0330 <jtp0330@gmail.com>
2024-04-17 23:31:05 -0700

    fixed contact animation bug;

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useInView, InView } from 'react-intersection-observer'
 import homestyle from '../css/Home.module.css'
 //utilize intersection observer to display welcome, then your infor card
@@ -6,35 +6,54 @@ import homestyle from '../css/Home.module.css'
 
 function Home() {
 
-    const observer = new IntersectionObserver((sections) => {
-        sections.forEach((section) => {
-            console.log(section)
-            if (section.isIntersecting) {
-                section.target.classList.add('screenVisible')
-            }
-            else {
-                section.target.classList.remove('screenVisible')
-            }
-        }
-        )
-    });
-    const invisibleElements = document.querySelectorAll(".screenHidden")
-    invisibleElements.forEach((element) => { observer.observe(element) });
 
+    const [isIntersecting, setisIntersecting] = useState(false);
+    const ref = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setisIntersecting(entry.isIntersecting);
+            },
+            { rootMargin: "-300px" }
+        );
+        console.log(isIntersecting)
+        observer.observe(ref.current);
+        return () => observer.disconnect();
+    }, [isIntersecting]);
+
+
+    useEffect(() => {
+        if (isIntersecting) {
+            ref.current.querySelectorAll("div").forEach((element) => {
+                element.classList.add("fadeIn");
+            });
+        } else {
+            ref.current.querySelectorAll("div").forEach((element) => {
+                element.classList.remove("fadeIn");
+            });
+        }
+    }, [isIntersecting])
 
     return (
         <div className={homestyle.home_page}>
-            <h1 className={homestyle.screenVisible}>Welcome</h1>
-            <div className={homestyle.introduction + ' ' + homestyle.screenHidden}>
-                <img src="./src/assets/images/Portrait.jpeg" alt="My Portrait" width="400" height="550"></img>
-                <div>
-                    <p>My name, is Jared Park.
-                        <br />
-                        <br />I am a SWE | IT Support | Cloud Operations
-                        <br />with an intense interest in Cloud Computing.
-                    </p>
+            <header>
+                <h1 className={homestyle.screenVisible}>Welcome</h1>
+            </header>
+            <main>
+                <div className={homestyle.introduction + ' ' + homestyle.screenHidden} ref={ref}>
+                    <img src="./src/assets/images/Portrait.jpeg" alt="My Portrait" width="400" height="550"></img>
+                    <div>
+                        <p>My name, is Jared Park.
+                            <br />
+                            <br />I am a SWE | IT Support | Cloud Operations
+                            <br />with an intense interest in Cloud Computing.
+                        </p>
+                    </div>
                 </div>
-            </div>
+            </main>
         </div>
     )
+
+
 } export default Home

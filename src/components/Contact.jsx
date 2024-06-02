e324ac8bf3a46b5e1d211d809e4ab4aa356dfd71
jtp0330 <jtp0330@gmail.com>
2024-06-01 23:32:17 -0700

    started email form for contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,10 @@ import react from 'react'
 import contactstyle from '../css/Contact.module.css'
 
 const Contact = () => {
+
+    const sendEmail = () => {
+        alert("Email has been sent!")
+    }
     return (
         <div className={`${contactstyle.contact} text-center`}>
             <h1>Let's Get In Touch!</h1>
@@ -12,6 +16,17 @@ const Contact = () => {
                 <li><a href="https://www.linkedin.com/in/jared-park-computer-scientist159/">LinkedIn</a></li>
                 <li><a href="https://github.com/jtp0330">GitHub</a></li>
             </ul>
+            <form>
+                <label for="">First Name</label>
+                <input type="text"></input>
+                <label for="text">Last Name</label>
+                <input></input>
+                <label for="text">Email</label>
+                <input></input>
+                <label for="textarea">Message</label>
+                <input></input>
+                <input type="submit" onSubmit={sendEmail()} />
+            </form>
         </div >
     );
 };

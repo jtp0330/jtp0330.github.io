38e55ed1659a0c9d261671cac91973da513b25fd
jtp0330 <jtp0330@gmail.com>
2024-07-25 23:36:46 -0700

    some css changes

--- src/components/projects_showcase/Showcase.jsx
+++ src/components/projects_showcase/Showcase.jsx
@@ -16,7 +16,7 @@ const Showcase = () => {
             <div className={showcasestyle.projects}>
                 <Project className={showcasestyle.project} projectName="BrightIdeas" projectDescription="A full-stack application, designed with MERN to create a collaborative website for users to share and manage their ideas." projectLink="https://github.com/jtp0330/brightIdeas" onClick={handlePopUp()} />
                 <Project className={showcasestyle.project} projectName="Bread" projectDescription="Mobile app for users to help manage budgeting" projectLink="https://github.com/jtp0330/Bread-app" />
-                <Project className={showcasestyle.project} projectName="Test Project" projectDescription="This a test" projectLink="Test Link" />
+                <Project className={showcasestyle.project} projectName="Recipe Management" projectDescription="Application that allows save, upload and manage their recipes" projectLink="https://github.com/jtp0330/recipeManagement" />
             </div>
         </div>
 

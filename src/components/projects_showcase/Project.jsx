650d5266b07710eddcc91ba61d241511954d9c79
jtp0330 <jtp0330@gmail.com>
2024-07-28 19:23:33 -0700

    added additional prompt for card image property of project section

--- src/components/projects_showcase/Project.jsx
+++ src/components/projects_showcase/Project.jsx
@@ -1,15 +1,14 @@
-import react from 'react'
 import projectstyle from '../../css/Project.module.css'
 import Card from 'react-bootstrap/Card'
 
 const Project = (props) => {
 
     //destructuring
-    const { projectName, projectDescription, projectLink } = props;
+    const { projectName, projectDescription, projectLink, projectImage } = props;
 
     return (
-        <Card className={`${projectstyle.projectContent} hover-zoom hover-shadow bg-dark text-light`}border="light">
-            <Card.Img variant="top" src="#" alt="Project picture" />
+        <Card className={`${projectstyle.projectContent} hover-zoom hover-shadow bg-dark text-light`} border="light">
+            <Card.Img className={projectstyle.projectImages} variant="top" src={`${projectImage}`} alt="Project picture" />
             <Card.Body>
                 <div className={`${projectstyle.projectText}`}>
                     <Card.Title><h3 id="project_header">{projectName}</h3></Card.Title>

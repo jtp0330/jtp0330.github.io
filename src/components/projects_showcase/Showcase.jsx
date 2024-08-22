a1484723827bd7232684de9be1b274e3b07a0130
jtp0330 <jtp0330@gmail.com>
2024-08-21 22:26:55 -0700

    changed icon

--- src/components/projects_showcase/Showcase.jsx
+++ src/components/projects_showcase/Showcase.jsx
@@ -14,9 +14,10 @@ const Showcase = () => {
         <div className={`${showcasestyle.showcase} p-5`}>
             <h1>My Projects</h1>
             <div className={showcasestyle.projects}>
-                <Project className={showcasestyle.project} projectName="BrightIdeas" projectDescription="A full-stack application, designed with MERN to create a collaborative website for users to share and manage their ideas." projectLink="https://github.com/jtp0330/brightIdeas" projectImage="#" onClick={handlePopUp()} />
-                <Project className={showcasestyle.project} projectName="Bread" projectDescription="Mobile app for users to help manage budgeting" projectLink="https://github.com/jtp0330/Bread-app" projectImage="./src/images/breadicon.png" />
-                <Project className={showcasestyle.project} projectName="Recipe Management" projectDescription="Application that allows save, upload and manage their recipes" projectLink="https://github.com/jtp0330/recipeManagement" projectImage="#" />
+                <Project className={showcasestyle.project} projectName="BrightIdeas" projectDescription="Full-stack application, designed with MERN to create a collaborative website for users to share and manage their ideas." projectLink="https://github.com/jtp0330/brightIdeas" projectImage="#" onClick={handlePopUp()} />
+                <Project className={showcasestyle.project} projectName="Bread" projectDescription="Mobile application, allowing users to create, and manage their budget goals" projectLink="https://github.com/jtp0330/Bread-app" projectImage="./src/images/breadicon.png" />
+                {/* <Project className={showcasestyle.project} projectName="Recipe Management" projectDescription="Application that allows save, upload and manage their recipes" projectLink="https://github.com/jtp0330/recipeManagement" projectImage="#" /> */}
+                <Project className={showcasestyle.project} projectName="Overcast" projectDescription="2D Platformer, where you play as a winged pig, soaring through the clouds, spreading rainbows and sunshine" projectLink="https://github.com/jtp0330/Overcast" projectImage="./src/images/overcast.png" />
             </div>
         </div>
 
